c5ec35c5f7b5abfa9f55d3bbf507a64359e9372a
Curtis Netterville <curtis@netterville.com>
2025-09-16 21:05:32 -0500

    Initial commit for checkpoints

---
+++ WAN Monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WAN Monitor
+//
+//  Created by Curtis Netterville on 9/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WAN Monitor/WAN_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAN_MonitorApp.swift
+//  WAN Monitor
+//
+//  Created by Curtis Netterville on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WAN_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
